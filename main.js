b5761c7d3b001693462b67f662723b1bff89c1b0
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2024-12-25 14:43:23 -0600

    add operate function

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-function add(a, b) {
-    return a + b;
+let operator = "";
+let num1 = "";
+let num2 = "";
+
+function add(num1, num2) {
+    return parseFloat(num1) + parseFloat(num2);
 }
-function subtract(a, b) {
-    return a - b;
+function subtract(num1, num2) {
+    return parseFloat(num1) - parseFloat(num);
 }
-function multiply(a, b) {
-    return a * b;
+function multiply(num1, num2) {
+    return parseFloat(num1) * parseFloat(num2);
 }
-function divide(a, b) {
-    return a % b;
+function divide(num1, num2) {
+    return parseFloat(num1) / parseFloat(num2);
+}
+
+
+function operate(operator, num1, num2) {
+    switch(operator)
+        case '+':
+        return add(num1, num2);
+        break;
+
+        case '-':
+        return subtract(num1, num2);
+        break;
+
+        case '*':
+        return multiply(num1, num2);
+        break;
+
+        case '/':
+        return divide(num1, num2);
+
 }
